9c31d6fcf8f3c6b1c21570b0fcc45b052de93ea4
Alex Soucy <asoucy22@yahoo.com>
2021-12-05 23:21:35 -0500

    added initial inquirer functionality with questions to capture user input

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'readMeTitle',
+            message: 'What is the title of the project?:'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Write a brief description of the project:'
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Describe the process for installation:',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is the project used for?:'
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Who contributed to this project?:'
+        },
+        {
+            type: 'confirm',
+            name: 'testingConfirm',
+            message: 'Is there testing involved with this project?:',
+            default: false
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'Choose the correct license for your project (if applicable):',
+            choices: ['Academic', 'GNU', 'ISC', 'MIT', 'Mozilla', 'Open']
+        },
+        {
+            type: 'input',
+            name: 'profileName',
+            message: 'Enter your GitHub username:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your e-mail address for people to contact you with questions:'
+        }
+    ]);
+}
 
+promptUser();
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
