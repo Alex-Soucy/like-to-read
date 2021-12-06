18371b53dd5873bdca739bc5d511d2751739a992
Alex Soucy <asoucy22@yahoo.com>
2021-12-05 23:38:47 -0500

    added initial code to generateMarkdown.js to layout the README file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,33 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.readMeTitle}
+
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * 
+
+  ## Description
+  ${data.description}
+
+  ### Installation
+  ${data.install}
+
+  ### Usage
+  ${data.usage}
+
+  ### Contributors
+  ${data.contribution}
+
+  ### Questions
+
+  Links to my GitHub page and e-mail will be provided below for any questions you may have:
+
+  GitHub: [${data.profileName}](https://github.com/${data.profileName})
+
+  E-mail: ${data.email}
 
 `;
 }
